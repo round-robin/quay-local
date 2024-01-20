0a1a776d169bf8eeaec3626e6463648a6dbd0dea
Round Robin <16824801+round-robin@users.noreply.github.com>
2024-01-20 10:45:11 +0100

    Autodiscover the IPs of the services

--- quay-setup.sh
+++ quay-setup.sh
@@ -6,7 +6,7 @@ if [ ! $(whoami) = "root" ]; then
 fi
 
 QUAY=$(pwd)
-CONTAINER_TOOL=docker
+export CONTAINER_TOOL=docker
 
 if [ ! -d "$QUAY/postgres" ]; then
   mkdir postgres
@@ -15,10 +15,18 @@ fi
 setfacl -m u:26:-wx $QUAY/postgres
 setfacl -m u:1001:-wx $QUAY/storage
 
-$CONTAINER_TOOL run -d --rm --name quaydb -e POSTGRES_USER=quay -e POSTGRES_PASSWORD=quay -e POSTGRES_DB=quay -p 5432:5432 -v $QUAY/postgres:/var/lib/postgresql/data:Z postgres:10.12
+$CONTAINER_TOOL network create --driver bridge quaynet 
+
+$CONTAINER_TOOL run -d --rm --name quaydb --network quaynet -e POSTGRES_USER=quay -e POSTGRES_PASSWORD=quay -e POSTGRES_DB=quay -p 5432:5432 -v $QUAY/postgres:/var/lib/postgresql/data:Z postgres:10.12
 sleep 15
+QUAY_DB_IP=$($CONTAINER_TOOL inspect quaydb -f "{{ .NetworkSettings.Networks.quaynet.IPAddress }}")
 $CONTAINER_TOOL exec -it quaydb /bin/bash -c 'echo "CREATE EXTENSION IF NOT EXISTS pg_trgm" | psql -d quay -U quay'
-$CONTAINER_TOOL run -d --rm --name quay-redis -p 6379:6397 redis:5.0.7 --requirepass strongpassword
-#$CONTAINER_TOOL run -it --rm --name quay-config -p 8081:8080 quay.io/projectquay/quay config secret
+
+$CONTAINER_TOOL run -d --rm --name quay-redis --network quaynet -p 6379:6397 redis:5.0.7 --requirepass strongpassword
+QUAY_REDIS_IP=$($CONTAINER_TOOL inspect quay-redis -f "{{ .NetworkSettings.Networks.quaynet.IPAddress }}")
+
+#$CONTAINER_TOOL run -it --rm --name quay-config --network quaynet -p 8081:8080 quay.io/projectquay/quay config secret
 # Go to the web browser http://localhost:8081 and create a configuration of the service
-$CONTAINER_TOOL run --rm -d -p 8081:8080 --name quay --privileged=true -v $QUAY/config:/conf/stack:Z -v $QUAY/storage:/datastorage:Z quay.io/projectquay/quay:latest
+
+cat config/config.yaml.tmpl | sed -e "s/QUAY_DB_IP/$QUAY_DB_IP/" -e "s/QUAY_REDIS_IP/$QUAY_REDIS_IP/" > config/config.yaml
+$CONTAINER_TOOL run --rm -d -p 8081:8080 --name quay --privileged=true --network quaynet -v $QUAY/config:/conf/stack:Z -v $QUAY/storage:/datastorage:Z quay.io/projectquay/quay:latest
